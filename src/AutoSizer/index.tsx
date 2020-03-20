357568f939b9a7a2124e8a3bdf8b04fc6be830e6
Quiñónez, Carlo J <carlo.quinonez@thermofisher.com>
2020-03-19 18:44:55 -0700

    fix(ui-kit): set AutoSizer disableheight to true

--- src/AutoSizer/index.tsx
+++ src/AutoSizer/index.tsx
@@ -6,10 +6,13 @@ import VirtualizedSizer, { AutoSizerProps } from 'react-virtualized-auto-sizer';
  */
 
 // we wrap autosize to ensure that that the parent container will always have a height!!
-export const AutoSizer: React.FunctionComponent<AutoSizerProps> = props => (
-  <div className="flex h-full">
-    <div className="flex-auto">
-      <VirtualizedSizer {...props} />
+export const AutoSizer: React.FunctionComponent<AutoSizerProps> = props => {
+  props.disableHeight = true;
+  return (
+    <div className="flex h-full">
+      <div className="flex-auto">
+        <VirtualizedSizer {...props} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
